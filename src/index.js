38422fde2622860a2f40c9d05fc307534dcca684
2217881438 <2217881438@qq.com>
2022-02-18 10:32:13 +0800

    框架1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
 import './index.css';
 import Life from './pages/demo/Life';
+import Admin from './admin';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Life />
+    <Admin />
     {/* <App></App> */}
   </React.StrictMode>,
   document.getElementById('root')
