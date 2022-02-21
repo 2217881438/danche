49e0c6493f5d8700c21f0e6ac73742b489b92a79
2217881438 <2217881438@qq.com>
2022-02-21 14:03:38 +0800

    准备插入实时时间

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Row, Col } from "antd";
+import './index.less'
+// import Util from '../../utils/utils'
 
 export default class Header extends React.Component{
+    // state={}
+    componentWillMount(){
+        this.setState({
+            userName:'河畔一角'
+        })
+        
+    }
     render(){
         return (
-            <div>
-                This is Header
+            <div className='header'>
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎,{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4" className="breadcrumb-title">
+                        首页
+                    </Col>
+                    <Col span="20" className="weater">
+                        {/* <span className="data">{this.state.sysTime}</span> */}
+                        <span className="data">2022</span>
+                        <span className="weather-detail">晴转多云</span>
+                    </Col>
+                </Row>
             </div>
         )
     }
