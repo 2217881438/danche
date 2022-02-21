2ab4f54c533301434f249023775f66896539d2d8
2217881438 <2217881438@qq.com>
2022-02-21 15:30:33 +0800

    天气组件

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Row, Col } from "antd";
 import './index.less'
-// import Util from '../../utils/utils'
+import Util from '../../utils/utils'
+import axios from '../../axios'
 
 export default class Header extends React.Component{
-    // state={}
+    state={}
     componentWillMount(){
         this.setState({
             userName:'河畔一角'
         })
-        
+        setInterval(()=>{
+            let sysTime = Util.formateDate(new Date().getTime());
+            this.setState({
+                sysTime
+            })
+        },1000)
+        this.getWeatherAPIDate();
+    }
+    getWeatherAPIDate(){
+        let city = '北京';
+        axios.jsonp({
+            url:'https://free-api.heweather.net/s6/weather/now?location='+encodeURIComponent(city)+'&key=db86a5196f304e52a4369818c5182e60'
+        }).then((res)=>{
+            debugger;
+            if(res.status == 'success'){
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            }
+        })
     }
     render(){
         return (
@@ -25,9 +47,12 @@ export default class Header extends React.Component{
                         首页
                     </Col>
                     <Col span="20" className="weater">
-                        {/* <span className="data">{this.state.sysTime}</span> */}
-                        <span className="data">2022</span>
-                        <span className="weather-detail">晴转多云</span>
+                        <span className="data">{this.state.sysTime}</span>
+                        {/* <span className="data">2022</span> */}
+                        <span className="weather-detail">
+                            <img src={this.state.dayPictureUrl} alt="" />
+                            {this.state.weather}
+                        </span>
                     </Col>
                 </Row>
             </div>
